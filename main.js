9e1ba671f20887e5a8e2107b7ca7efd822a13168
aleherrera-9 <aleherrera.9.m@gmail.com>
2021-10-25 18:30:51 -0300

    This is a new commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let nombre=prompt("Ingrese su nombre");
+alert("Bienvenid@ "+nombre);
+let curso=prompt("Ingrese el curso");
+alert(nombre+ " esta cursando "+curso);
+console.log("nombre: "+nombre);
+console.log("curso: "+curso);
